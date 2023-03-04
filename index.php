4e353f2315c11ddfc9ff7921b5ff0ef942818c8d
Ali-Ayb <aliayoub.me1@gmail.com>
2023-03-04 17:17:38 +0200

    added email and password validation api

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+$emailPattern = "/^[^\s@]+@[^\s@]+\.[^\s@]+$/";
+$passwordPattern = "/^(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z]).{8,}$/";
+
+$emailValid = preg_match($emailPattern, $email);
+$passwordValid = preg_match($passwordPattern, $password);
+
+$response = [
+    "valid" => $emailValid && $passwordValid
+];
+
+echo json_encode($response);
