38549bd6befeac5681125d1e56a150ae8a870513
Ali-Ayb <aliayoub.me1@gmail.com>
2023-03-06 01:42:21 +0200

    added reverse numbers api

---
+++ reverse-numbers-api.php
@@ -0,0 +1,24 @@
+<?php
+
+
+// $string = $_GET['string'];
+
+$string = 'hello123';
+$numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+$temp_arr = [];
+
+for ($index = 0; $index < strlen($string); $index++) {
+    if (in_array($string[$index], $numbers)) {
+        array_push($temp_arr, $string[$index]);
+    }
+}
+
+$temp_arr = array_reverse($temp_arr);
+
+for ($index = 0; $index < strlen($string); $index++) {
+    if (in_array($string[$index], $numbers)) {
+        $string[$index] = array_shift($temp_arr);
+    }
+}
+
+echo $string;
